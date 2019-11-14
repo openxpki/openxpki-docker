97201201ed790cc30fbdcde139b5103463a5838a
Michael Jungmair <michael.jungmair@cs.tum.edu>
2019-11-14 14:40:29 +0100

    adapt setup-cert.sh for easier use (#3)
    * adapt setup-cert.sh for easier use
    
    * change cp to cp -n to not override key files by accident

--- bin/setup-cert.sh
+++ bin/setup-cert.sh
@@ -7,46 +7,38 @@ function alias_for_identifier() {
   ret="$(openxpkiadm alias --realm "$1" | perl -0777 -ne "\$str=\$_; print \$str =~ /Alias\s*:\s*(.*)\s*\n\s*Identifier\s*:\s*$2/ ;")"
 }
 # carries out the whole import process
-# Parameters: [1]: realm (e.g. "ca-one") [2]: path to certificate file [3]: list of all certificates of realm
+# Parameters: [1]: realm (e.g. "ca-one") [2]: path to certificate file [3]: certificate type
 function import_cert() {
   realm="$1"
   file="$2"
   type="$3"
   echo "detected $type certificate for realm '$realm': $(basename "$file")"
+  #expected location of keyfile
   key_file="$BASE_PATH/$realm/$(basename "$file" .crt).pem"
-  # if certificate is not root certificate, it needs a corresponding key file
-  if [ -f "$key_file" ] || [ "$type" = "root" ]; then
-    # calculate identifier for certificate
-    identifier="$(openxpkiadm certificate id --file "$file")"
-    # check if certificate is already imported
-    if openxpkicli get_cert --realm "$realm" --arg identifier="$identifier" >/dev/null 2>/dev/null; then
-      echo "IGNORING $file as it is already imported"
-    else
-      # import certificate depending on its type
-      if [ "$type" = "root" ]; then
-        openxpkiadm certificate import --file "$file"
-        # No key file for root certificate
-
-      elif [ "$type" = "vault" ]; then
-        openxpkiadm certificate import --file "$file" --realm "$realm" --token datasafe
-        alias=""
-        alias_for_identifier "$realm" "$identifier" alias
-        # key file of vault can not be moved to database -> rename it such that openxpki can find it
-        cp "$key_file" "$BASE_PATH/$realm/$alias.pem"
-      else
-        openxpkiadm certificate import --file "$file" --realm "$realm" --token certsign
-        alias=""
-        alias_for_identifier "$realm" "$identifier" alias
-        # insert certificate key into database (this requires an active openxpki server instance)
-        openxpkicli set_data_pool_entry --arg namespace=sys.crypto.keys \
-          --arg key="$alias" \
-          --arg encrypt=1 \
-          --filearg value="$key_file" --realm "$realm"
-      fi
-    fi
+  #extract optional generation parameter which is part of the file name
+  generation="$( echo "$file" | sed -rn 's/.*-([0-9]+)\.crt/\1/p')"
+  # calculate identifier for certificate
+  identifier="$(openxpkiadm certificate id --file "$file")"
+  # check if certificate is already imported
+  if openxpkicli get_cert --realm "$realm" --arg identifier="$identifier" >/dev/null 2>/dev/null; then
+    echo "IGNORING $file as it is already imported"
   else
-    echo "IGNORING '$realm'/$(basename "$file"): No matching key file exists"
+      #import certificate with/without generation
+      if [ ! -z "$generation" ]; then
+        openxpkiadm certificate import --file "$file" --realm "$realm" --token "$type" --generation $generation
+      else
+        openxpkiadm certificate import --file "$file" --realm "$realm" --token "$type"
+      fi
+      if [ ! -f "$key_file" ]; then
+        echo "WARNING: '$realm'/$(basename "$file"): No matching key file exists"
+      else
+        #move keyfile to the right place so that it can be found by openxpki
+        alias=""
+        alias_for_identifier "$realm" "$identifier" alias
+        cp -n "$key_file" "$BASE_PATH/$realm/$alias.pem"
+      fi
   fi
+
 }
 
 function do_realm_dir() {
@@ -54,43 +46,47 @@ function do_realm_dir() {
   realm="$(basename "$realm_dir")"
   if openxpkiadm certificate list --realm "$realm" >/dev/null; then
     # regular expressions for finding the right files
-    root_regex=".*/\(.*_\)*$(echo "$realm")_root_ca\(_.*\)*\.crt"
-    vault_regex=".*/\(.*_\)*$(echo "$realm")_datavault\(_.*\)*\.crt"
-    issuing_regex=".*/\(.*_\)*$(echo "$realm")_issuing_ca\(_.*\)*\.crt"
+    root_regex=".*/root\(-[0-9]*\)\{0,1\}\.crt"
+    vault_regex=".*/vault\(-[0-9]*\)\{0,1\}\.crt"
+    issuing_regex=".*/ca-signer\(-[0-9]*\)\{0,1\}\.crt"
+    scep_regex=".*/scep\(-[0-9]*\)\{0,1\}\.crt"
 
-    vault_cert_location="global"
-    # start import process for detected root/vault/signing certificates
-    for f in $(find "$realm_dir" -mindepth 1 -maxdepth 1 -type f -regextype sed -iregex "$root_regex"); do
-      import_cert "$realm" "$f" "root"
-    done
-    for f in $(find "$realm_dir" -mindepth 1 -maxdepth 1 -type f -regextype sed -iregex "$vault_regex"); do
-      import_cert "$realm" "$f" "vault"
-      vault_cert_location="local"
-    done
-
-    # if no local datavault certificate/key was found: look for global cert/key in ../
-    if [ "$vault_cert_location" = "global" ]; then
-      echo "searching for global vault certificate"
-      for f in $(find "$BASE_PATH" -mindepth 1 -maxdepth 1 -type f -regextype sed -iregex "$vault_regex"); do
+    # look for global vault cert/key in ../
+    for f in $(find "$BASE_PATH" -mindepth 1 -maxdepth 1 -type f -regextype sed -iregex "$vault_regex"); do
         key_file="$BASE_PATH/$(basename "$f" .crt).pem"
         if [ -f "$key_file" ]; then
-          # import vault certificate globally
-          openxpkiadm certificate import --file "$f"
-          #set realm alias
-          vault_identifer="$(openxpkiadm certificate id --file "$f")"
-          openxpkiadm alias --realm "$realm" --token datasafe --identifier $vault_identifer
-          #get alias name for imported certificate
-          vault_alias=""
-          alias_for_identifier "$realm" "$vault_identifer" vault_alias
-          #copy vault key to realm folder (otherwise openxpki can not find it)
-          cp "$key_file" "$BASE_PATH/$vault_alias.pem"
+          vault_identifier="$(openxpkiadm certificate id --file "$f")"
+          if openxpkicli get_cert --realm "$realm" --arg identifier="$vault_identifier" >/dev/null 2>/dev/null; then
+            echo "vault certificate already imported"
+          else
+            # import vault certificate globally
+            openxpkiadm certificate import --file "$f"
+            #set realm alias
+            openxpkiadm alias --realm "$realm" --token datasafe --identifier "$vault_identifier"
+            alias=""
+            alias_for_identifier "$realm" "$identifier" alias
+            cp -n "$key_file" "$BASE_PATH/$alias.pem"
+          fi
         else
           echo "IGNORING $(basename "$f"): No matching key file exists"
         fi
-      done
-    fi
+    done
+    #import root certificate
+    for f in $(find "$realm_dir" -mindepth 1 -maxdepth 1 -type f -regextype sed -iregex "$root_regex"); do
+      root_identifer="$(openxpkiadm certificate id --file "$f")"
+      if openxpkicli get_cert --realm "$realm" --arg identifier="$root_identifer" >/dev/null 2>/dev/null; then
+        echo "root certificate already imported"
+      else
+        echo "importing root"
+        openxpkiadm certificate import --file "$f"
+      fi
+    done
+    #generic import of certsign/scep tokens
+    for f in $(find "$realm_dir" -mindepth 1 -maxdepth 1 -type f -regextype sed -iregex "$scep_regex"); do
+      import_cert "$realm" "$f" "scep"
+    done
     for f in $(find "$realm_dir" -mindepth 1 -maxdepth 1 -type f -regextype sed -iregex "$issuing_regex"); do
-      import_cert "$realm" "$f" "signer"
+      import_cert "$realm" "$f" "certsign"
     done
 
   else
